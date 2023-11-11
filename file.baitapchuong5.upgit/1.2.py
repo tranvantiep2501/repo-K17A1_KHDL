91e2e152e84a1703ed523a4731f938f7fa23b8d4
tranvantiep2501 <148097757+tranvantiep2501@users.noreply.github.com>
2023-11-11 22:14:28 +0700

    Add files via upload

---
+++ file.baitapchuong5.upgit/1.2.py
@@ -0,0 +1,11 @@
+x=10
+y=5
+c=x+y
+d=x-y
+e=x*y
+f=x/y
+print(x, '=', 10, y, '=', 5)
+print('tổng', x, '+', y, '=', c )
+print('hiệu', x, '-', y, '=', d)
+print('tích', x, '*', y, '=', e)
+print('thương', x, '/', y, '=', f)
